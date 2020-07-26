2269478e6ed7f41be559b496eeaac6b6b982e101
DarkTofts <darksstofts@gmail.com>
2020-07-26 20:13:29 +0100

    Changes made by Simon

--- app/modules/job/list/list.controller.js
+++ app/modules/job/list/list.controller.js
@@ -8,5 +8,12 @@
     function JobListController(jobCollectionResponse) {
         var vm = this;
         vm.jobCollection = jobCollectionResponse;
+        // Create an array of the name values of the status ID's
+        // Then in the template apply the ID to the array to get the text values
+        // Better way of doing this would be to convert the int to a string value in the API
+        // I looked into to this found some one on StackOver flow suggesting creating a static method in the 
+        // Entity , but not Documenation to back this up.
+        // With more time I would research a way of applying this.
+        vm.stautsNames = [ 'Open', 'Progress' , 'Closed'];
     }
 })();
